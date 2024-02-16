0bd8e8876f99e91b9ef77c43bd460297d51b7828
Arshaq <arshahm2004@gmail.com>
2024-02-16 19:15:53 +0000

    finished

--- copy.cpp
+++ copy.cpp
@@ -1,9 +1,41 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
-int main() {
-    
+int main(int argc, char* argv[]) {
+    // Check if the correct number of command line arguments is provided
+    if (argc != 3) {
+        cerr << "Usage: " << argv[0] << " <source filename> <destination filename>" << endl;
+        return 1; // Exit with error
+    }
+
+    // Open the source file for reading
+    ifstream sourceFile(argv[1]);
+    if (!sourceFile) {
+        cerr << "Error: Unable to open source file." << endl;
+        return 1; // Exit with error
+    }
+
+    // Open the destination file for writing
+    ofstream destFile(argv[2]);
+    if (!destFile) {
+        cerr << "Error: Unable to open destination file." << endl;
+        return 1; // Exit with error
+    }
+
+    // Copy contents from source file to destination file
+    string line;
+    while (getline(sourceFile, line)) {
+        destFile << line << endl;
+    }
+
+    // Close the files
+    sourceFile.close();
+    destFile.close();
+
+    cout << "File copied successfully." << endl;
 
     return 0;
 }
